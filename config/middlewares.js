447fa305b8324336342cf42a087b94ece14b5450
Sean Davis <seandavis@Seans-MacBook-Pro.local>
2022-06-10 08:18:48 -0400

    initial commit

---
+++ config/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            process.env.STORAGE_URL,
+            process.env.STORAGE_CDN_URL,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            process.env.STORAGE_URL,
+            process.env.STORAGE_CDN_URL,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
